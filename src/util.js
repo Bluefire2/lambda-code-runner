58b43caa667e8f59c05ce42c4ba171754076c9b2
Kirill Chernyshov <kc875@cornell.edu>
2019-04-19 23:44:33 -0400

    Move processing stuff

--- src/util.js
+++ src/util.js
@@ -3,6 +3,24 @@ export const DIRECTION = {
     BACKWARD: "BACKWARD"
 };
 
+export const MOVE = {
+    TYPE: {
+        TAKE: "take",
+        MOVE: "move",
+        SPAWN: "spawn"
+    },
+    DIRECTION: {
+        N: "N",
+        NE: "NE",
+        NW: "NW",
+        S: "S",
+        SE: "SE",
+        SW: "SW",
+        E: "E",
+        W: "W"
+    }
+};
+
 export const pending = name => name + "_PENDING";
 export const fulfilled = name => name + "_FULFILLED";
 export const rejected = name => name + "_REJECTED";
@@ -24,5 +42,59 @@ export const arrayBufferToString = buf =>
  */
 export const processMove = (board, move, forward) => {
     // TODO: implement
+    const {type, team, handle} = move;
+    switch (type) {
+        case MOVE.TYPE.MOVE:
+            // move robot
+            const {direction} = move,
+                robot = board.robots[handle],
+                [dx, dy] = directionToCoordinates(direction),
+                [x, y] = robot.xy;
+            // update co-ordinates, checking bounds
+            if (isBetween(0, board.width - 1, x + dx)
+                && isBetween(0, board.height, y + dy)) {
+                robot.xy = [x + dx, y + dy];
+            }
+            break;
+        case MOVE.TYPE.TAKE:
+            // take gold
+            // TODO
+            break;
+        case MOVE.TYPE.SPAWN:
+            // spawn new robot
+            board.robots[handle] = {
+                team,
+                xy: [0, 0] // TODO: spawn on the team's home base
+            };
+            break;
+        default:
+            // aaaaaaaaa
+    }
     return board;
+};
+
+const isBetween = (lower, upper, x) => lower <= x && x <= upper;
+
+const directionToCoordinates = direction => {
+    // [dx, dy]
+    switch (direction) {
+        case MOVE.DIRECTION.N:
+            return [0, 1];
+        case MOVE.DIRECTION.NE:
+            return [1, 1];
+        case MOVE.DIRECTION.NW:
+            return [-1, 1];
+        case MOVE.DIRECTION.S:
+            return [0, -1];
+        case MOVE.DIRECTION.SE:
+            return [1, -1];
+        case MOVE.DIRECTION.SW:
+            return [-1, -1];
+        case MOVE.DIRECTION.E:
+            return [1, 0];
+        case MOVE.DIRECTION.W:
+            return [-1, 0];
+        default:
+            return [0, 0]; // ??????
+    }
 };
