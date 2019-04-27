ef92f5f80f4535aa0216987f7d0c71f1272e1a4d
Kirill Chernyshov <kc875@cornell.edu>
2019-04-27 19:08:04 -0400

    Code cleanup

--- src/App/Header/Header.js
+++ src/App/Header/Header.js
@@ -10,73 +10,6 @@ import "./style.css";
 
 
 class Header extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    renderScore(teams, scores) {
-
-        //check for null
-        if (teams == null || scores == null) {
-            return;
-        }
-        
-        var lst = []
-        lst.push(
-            <div className="section-head" key={-1}>Scores</div>
-        )
-        for (var i = 0; i < teams.length; i++) {
-            lst.push(
-                <div className="score-item" key={i}>
-                    Team {teams[i]}: {scores[teams[i]]}
-                </div> 
-            );
-        }
-        return lst;
-    }
-
-    renderBots(teams, bots, cap) {
-
-        //check for null
-        if (teams == null || bots == null || cap == null) {
-            return;
-        }
-
-        var lst = [];
-        lst.push(
-            <div className="section-head" key={-1}>Num of Bots</div>
-        )
-        for (var i = 0; i < teams.length; i++) {
-            lst.push(
-                <div className="bot-item" key={i}>
-                    Team {teams[i]}: {bots[i]}/{cap}
-                </div>
-            )
-        }
-        return lst;
-    }
-
-    renderMovesLeft(moves, next, goldcap) {
-        console.log(moves, next, goldcap);
-
-        //check for null
-        if (moves == null || next == null || goldcap == null) {
-            return;
-        }
-
-        var lst = [];
-        lst.push(
-            <div className="section-head" key={"gold"}>Gold Cap: {goldcap}</div>
-        );
-        lst.push(
-            <div className="section-head" key={"move-head"}>Moves Left: </div>
-        );
-        lst.push(
-            <div id="MovesCounter" key="move">{next} / {moves.length}</div>
-        )
-        return lst;
-    }
-
     render() {
         /*  teams: a list of all the teams
             scores: a list of the current scores for each team in order of the teams list
@@ -85,25 +18,44 @@ class Header extends Component {
             moves: all moves
             next: next index of move
         */
-        const {teamNames, teams, bots, max_gold, max_bots, moves, next} = this.props;
-        const score = this.renderScore(teamNames, teams);
-        //const numBots = this.renderBots(teams, bots, max_bots);
-        const movesLeft = this.renderMovesLeft(moves, next, max_gold);
+        const {teamNames, teams, bots, maxGold, maxBots, moves, next} = this.props;
+
+        if (teamNames === void 0) {
+            return (
+                <div id="header-container">
+                    <FieldFileInput className="header-item" label={"Import"} types={".json"} onChange={this.props.loadFile} />
+                </div>
+            );
+        }
+        // TODO: is it OK to use the index as the key here?
         return (
-            <div id="HeaderContainer">
+            <div id="header-container">
                 <FieldFileInput className="header-item" label={"Import"} types={".json"} onChange={this.props.loadFile} />
-                <div className="header-item" id="Score">{score}</div>
+                <div className="header-item" id="Score">
+                    <div className="section-head" key={-1}>Scores</div>
+
+                    {Object.keys(teams).map((team, i) =>
+                        <div className="score-item" key={i}>
+                            Team {team}: {teams[team]}
+                        </div>
+                    )}
+                </div>
+
                 {/* <div className="header-item" id="NumBots">{numBots}</div> */}
-                <div className="header-item" id="MovesLeft">{movesLeft}</div>
+                <div className="header-item" id="MovesLeft">
+                    <div className="section-head" key={"gold"}>Gold Cap: {maxGold}</div>
+                    <div className="section-head" key={"move-head"}>Moves Left: </div>
+                    <div id="MovesCounter" key="move">{next} / {moves.length}</div>
+                </div>
             </div>
         );
     }
 }
 
-const mapStateToProps = ({board: {teamNames, max_gold, max_bots, teams, robots},
+const mapStateToProps = ({board: {teamNames, maxGold, maxBots, teams, robots},
                           history: {moves, next}}) => {
     return {
-        teamNames, teams, robots, max_gold, max_bots, moves, next
+        teamNames, teams, robots, maxGold, maxBots, moves, next
     };
 };
 
