db5c7594f2538748ff1f86ca123004250c7e853b
Kirill Chernyshov <kc875@cornell.edu>
2019-05-01 01:20:07 -0400

    oops

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,7 @@
 import {combineReducers} from 'redux';
-import {reducer as formReducer} from 'redux-form';
 import boardReducer from "./boardReducer";
 
 const rootReducer = combineReducers({
-    form: formReducer,
     board: boardReducer
 });
 
