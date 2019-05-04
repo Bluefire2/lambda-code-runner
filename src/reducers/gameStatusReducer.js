4a1b6f1446726d398723bc96fdca52d52649a8f1
Kirill Chernyshov <kc875@cornell.edu>
2019-05-03 20:54:07 -0400

    Animation fixes

--- src/reducers/gameStatusReducer.js
+++ src/reducers/gameStatusReducer.js
@@ -1,11 +1,22 @@
+import produce from 'immer';
+
 import {fulfilled} from "../util";
 import {Action} from "../actions";
 
 export default (state = {initialized: false, play: false}, action) => {
     switch (action.type) {
         case fulfilled(Action.LOAD_FILE_ACTION):
-            const {play} = state;
-            return {initialized: true, play};
+            return produce(state, draft => {
+                draft.initialized = true;
+            });
+        case Action.SEQUENTIAL_MOVE_CYCLE:
+            return produce(state, draft => {
+                draft.play = true;
+            });
+        case Action.SEQUENTIAL_MOVE_CYCLE_STOP:
+            return produce(state, draft => {
+                draft.play = false;
+            });
         default:
             return state;
     }
